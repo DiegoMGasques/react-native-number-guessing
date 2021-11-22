fae9da738eb7f3788b48fc08a665ad59cafca676
Diego Gasques <diegomgasques@gmail.com>
2021-11-22 15:35:48 -0300

    Add Card presentational component

---
+++ components/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    // width: 300,
+    // maxWidth: "80%",
+    // alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    elevation: 4,
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 6,
+  },
+});
+
+export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+import Card from "../components/Card";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Start Game Screen</Text>
+      <Text style={styles.sectionTitle}>Start a new game!</Text>
+      <Card style={styles.card}>
+        <Text>Select a number!</Text>
+        <TextInput />
+        <View style={styles.buttonGroup}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </Card>
     </View>
   );
 };
@@ -14,6 +24,17 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  sectionTitle: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  card: { width: 300, maxWidth: "80%", alignItems: "center" },
+  buttonGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 0,
   },
 });
 
